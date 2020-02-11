517d7215f58897bbde73206ec600344df93db4d7
wzk1998 <869693441@qq.com>
2020-02-11 22:44:49 +0800

    update

---
+++ Week3/Elevator_2/src/Elevator2/EleRequireItem.java
@@ -0,0 +1,9 @@
+package Elevator2;
+
+public class EleRequireItem extends RequireItem{
+	public EleRequireItem(int targetFloor,int gotTime,int eleState) {
+		this.targetFloor = targetFloor;
+		this.gotTime = gotTime;
+		this.eleState = eleState;
+	}
+}
---
+++ Week3/Elevator_2/src/Elevator2/RequireItem.java
@@ -0,0 +1,22 @@
+package Elevator2;
+import java.util.*;
+
+public class RequireItem {
+	protected int targetFloor;
+	protected int gotTime;
+	protected int eleState;     //1是向上-1向下
+	
+	public RequireItem() {
+		
+	}
+	
+	public int getTargetFloor() {
+		return this.getTargetFloor();
+	}
+	public int getGotTime() {
+		return this.getGotTime();
+	}
+	public int getEleState() {
+		return this.eleState;
+	}
+}
