cc26619e46437d0c3e59bd2273500c025c794440
wzk1998 <869693441@qq.com>
2020-02-11 22:42:34 +0800

    update

--- Week3/Elevator_2/src/Elevator2/EleRequireItem.java
+++
@@ -1,9 +0,0 @@
-package Elevator2;
-
-public class EleRequireItem extends RequireItem{
-	public EleRequireItem(int targetFloor,int gotTime,int eleState) {
-		this.targetFloor = targetFloor;
-		this.gotTime = gotTime;
-		this.eleState = eleState;
-	}
-}
